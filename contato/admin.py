fdc952bdad2da0fb140838eebe633643ab006655
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-11 17:00:04 -0300

    atualizações

--- contato/admin.py
+++ contato/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contato
 
-# Register your models here.
+# Register your models here
+
+@admin.register(Contato)
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_de_criacao' )
+    ordering = '-id',
+    #list_filter = ('',)
+    search_fields = 'id', 'nome', 'sobrenome', 'telefone', 'email'
+    list_per_page = 20
+    list_display_links = ('id', 'nome', ) 
+    
--- contato/models.py
+++ contato/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50, blank=True)
+    telefone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, unique=True)
+    data_de_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True) 
+
+    def __str__(self):
+        return self.nome
