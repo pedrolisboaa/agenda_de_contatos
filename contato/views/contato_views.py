be905a791a5a6ab81991cfab16cdb70c9252dc0a
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-18 20:26:57 -0300

    finalizado o buscador

--- contato/views/contato_views.py
+++ contato/views/contato_views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contato.models import Contato
 from django.http import Http404
+from django.db.models import Q
 
 # Create your views here.
 
@@ -8,6 +9,28 @@ def index(request):
     contatos = Contato.objects.filter(mostrar=True).order_by('-id')
     context = {
         #'contatos': contatos,
+        'contatos': contatos
+    }
+    
+    return render(request, 'index.html', context)
+
+
+def buscar(request):
+    
+    valor_buscar = request.GET.get('q', '').strip()
+    if valor_buscar == '':
+        return(redirect('index'))
+    
+    contatos = Contato.objects \
+    .filter(mostrar=True) \
+    .filter(
+        Q(nome__icontains=valor_buscar) |
+        Q(telefone__icontains=valor_buscar) |
+        Q(nome__icontains=valor_buscar) |
+        Q(sobrenome__icontains=valor_buscar)) \
+    .order_by('id')
+
+    context = {
         'contatos': contatos
     }
     
