547660a6870c1da8e28e490e23f1bda07a60da94
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-20 14:25:54 -0300

    atualizando

--- contato/urls.py
+++ contato/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from .views import index, contato_unico, buscar
 
 urlpatterns = [ 
-    path('<int:id_contato>/', contato_unico, name='contato_unico'),
     path('buscar/', buscar, name='buscar'),
     path('', index, name='index'),
+
+    # Contato (CRUD)
+    path('contato/<int:id_contato>/detalhe/', contato_unico, name='contato_unico'),
+    #path('contato/<int:id_contato>/atualizar/', contato_unico, name='contato_unico'),
+    #path('contato/<int:id_contato>/detelar/', contato_unico, name='contato_unico'),
+    #path('contato/criar/', contato_unico, name='contato_unico'),
    
 ]
