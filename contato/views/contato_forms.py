b1928a3993ea99493fb21e808ca09d051994760b
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-22 20:23:15 -0300

    atualizando

--- contato/views/contato_forms.py
+++ contato/views/contato_forms.py
@@ -1,14 +1,24 @@
+from typing import Any
 from django.shortcuts import render, redirect
 from contato.models import Contato
-from django.http import Http404
-from django.db.models import Q
-from django.core.paginator import Paginator
+from contato.forms import ContatoForm
+
 
 # Create your views here.
 
+
 def criar(request):
-    context = {
+     
+    if request.method == 'POST':
+        context = {
+        'formulario': ContatoForm(request.POST)
+    }
         
+        return render(request, 'criar.html', context)
+         
+   
+    context = {
+        'formulario': ContatoForm()
     }
     
     return render(request, 'criar.html', context)
