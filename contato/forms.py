8fb6cfa404361545d5fc930d9f69cca87a7f1143
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-23 17:11:19 -0300

    atualizando|

--- contato/forms.py
+++ contato/forms.py
@@ -4,4 +4,13 @@ from .models import Contato
 class ContatoForm(forms.ModelForm):
     class Meta:
         model = Contato
-        fields = ('nome', 'sobrenome', 'telefone')
+        fields = ('nome', 'sobrenome', 'telefone', 'email', 'descricao', 'categoria')
+
+        widgets = {
+            'nome': forms.TextInput(
+                attrs={
+                    'class': 'classe-a classe-b',
+                    'placeholder': 'Informe seu nome',
+                }
+            )
+        }
