8528133ddb6444d3bd7ced2ccfe3ccfd9d4304ed
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-25 16:02:34 -0300

    atualizando

--- contato/forms.py
+++ contato/forms.py
@@ -2,9 +2,16 @@ from django import forms
 from .models import Contato
 
 class ContatoForm(forms.ModelForm):
+    imagem = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
     class Meta:
         model = Contato
-        fields = ('nome', 'sobrenome', 'telefone', 'email', 'descricao', 'categoria')
+        fields = ('nome', 'sobrenome', 'telefone', 'email', 'descricao', 'categoria', 'imagem')
 
         widgets = {
             'nome': forms.TextInput(
