1907dc9d8954faba7017d7e14626bbc8ea979117
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-26 14:36:57 -0300

    finalizando

--- contato/forms.py
+++ contato/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Contato
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 
@@ -26,4 +27,19 @@ class ContatoForm(forms.ModelForm):
         }
 
 class FormularioDeRegistro(UserCreationForm):
-    pass
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValueError('Já existe esse e-mail', code='invalid')
+            )
+            
+        return email
